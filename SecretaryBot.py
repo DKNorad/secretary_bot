a708c1411dbc0e47ca20217b208ddfae2cae3cfe
Ned Petrov <nedd.petrov@gmail.com>
2021-06-06 23:56:44 +0300

    Changed the file name and implemented a for loop

--- SecretaryBot.py
+++ SecretaryBot.py
@@ -4,11 +4,17 @@ from pathlib import Path
 from shutil import move
 
 image = ['*.bmp', '*.jpg', '*.jpeg', '*.gif', '*.png', '*.ico', '*.psd']
-video = ['*.mov', '*.wmv', '*.avi', '*.avchd', '*.flv', '*.f4v', '*.swf', '*.mkv', '*.webm', '*.mpg', '*.mpv', '*.mpe', '*.mpeg-2', '*.mpeg', '*.m4p', '*.m4v', '*.mp4', '*.ogg']
+video = ['*.mov', '*.wmv', '*.avi', '*.avchd', '*.flv', '*.f4v', '*.swf', '*.mkv', '*.webm',
+         '*.mpg', '*.mpv', '*.mpe', '*.mpeg-2', '*.mpeg', '*.m4p', '*.m4v', '*.mp4', '*.ogg']
 audio = ['*.mp3', '*.aif', '*.cda', '*.mid', '*.midi', '*.mpa', '*.wav', '*.wma', '*.wpl']
-document = ['*.txt', '*.ppt', '*.pptx', '*.ods', '*.xls', '*.xlsx', '*.pdf', '*.odt', '*.html', '*.htm', '*.doc', '*.docx', '*.pps', '*.odp', '*.xlsm']
+document = ['*.txt', '*.ppt', '*.pptx', '*.ods', '*.xls', '*.xlsx', '*.pdf', '*.odt', '*.html',
+            '*.htm', '*.doc', '*.docx', '*.pps', '*.odp', '*.xlsm']
 software = ['*.exe', '*.apk', '*.bat', '*.bin', '*.jar', '*.msi']
 archives = ['*.7z', '*.arj', '*.deb', '*.pkg', '*.rar', '*.rpm', '*.tar.gz', '*.z', '*.zip']
+
+folders = {'Images': image, 'Video': video, 'Audio': audio, 'Documents': document,
+           'Software': software, 'Archives': archives, 'Miscellaneous': '*.*'}
+
 total_moved = 0
 
 
@@ -16,7 +22,7 @@ def move_file(src: str, dst: str, pattern: list = '*.*'):
     global total_moved
     for ext in pattern:
         for f in fnmatch.filter(listdir(src), ext):
-            if path.isfile(f) and f != 'SecretaryBot.exe':
+            if path.isfile(f) and f != 'SecretaryBot.exe' and f != 'SecretaryBot.py':
                 if not path.isdir(dst):
                     Path(dst).mkdir(parents=True, exist_ok=True)
                 move(path.join(src, f), path.join(dst, f))
@@ -25,13 +31,8 @@ def move_file(src: str, dst: str, pattern: list = '*.*'):
 
 
 curr_dir = getcwd()
-move_file(curr_dir, curr_dir + '\\Images', image)
-move_file(curr_dir, curr_dir + '\\Video', video)
-move_file(curr_dir, curr_dir + '\\Audio', audio)
-move_file(curr_dir, curr_dir + '\\Documents', document)
-move_file(curr_dir, curr_dir + '\\Software', software)
-move_file(curr_dir, curr_dir + '\\Archives', archives)
-move_file(curr_dir, curr_dir + '\\Miscellaneous')
+for folder_name, file in folders.items():
+    move_file(curr_dir, curr_dir + f'\\{folder_name}', file)
 
 print(f'\nTotal files moved: {total_moved}')
 input("Press ENTER Key To Exit")
